2341af4a72b142b475b2f69cead512ea42402d01
CamSkiTheDev <cameronlucasaim@gmail.com>
2020-08-01 09:18:30 -0600

    initial server config

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// Connect to the database
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (error) =>
+        error ? console.log(error) : console.log('Connected to database...')
+)
+
+const app = express()
+const port = process.env.SERVER_PORT || 4001
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.status(200).send('Hello from server.'))
+
+app.listen(port, (error) =>
+    error ? console.log(error) : console.log('Server running on port:', port)
+)
